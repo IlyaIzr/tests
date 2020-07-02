8fda1441fa21cdf50781a2b82b03cf3b379e1b19
IlyaIzr <50058726+IlyaIzr@users.noreply.github.com>
2020-07-02 20:47:51 +0300

    Add files via upload

---
+++ ReactForm/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import FormExample from './FormExample/FormExample';
+import { Container } from 'semantic-ui-react';
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+
+        <FormExample />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
